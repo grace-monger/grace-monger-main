88e04c2662faaed8597244885f882d29d9cfea04
Jackie Manginelli <jmanginelli11@gmail.com>
2022-11-08 16:46:25 -0500

    Continuing my changes

--- client/components/AllWines.js
+++ client/components/AllWines.js
@@ -1,37 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchWines } from "../store/wines";
 
-export class AllWines extends Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    this.props.fetchWines();
-  }
-
-  render() {
-    const { allOfMyWines } = this.props;
-    return (
-      <div className="element-list">
-        {allOfMyWines.map((wine) => {
-          return (
-            <article key={wine.id} className="single-element">
-              <Link to={`/wines/${wine.id}`} key={wine.id}>
-                <img width="150px" src={wine.imageUrl} />
-                <h2>{wine.name}</h2>
-              </Link>
-              <div>
-                <button className="add-to-cart">Add to cart</button>
-              </div>
-            </article>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const AllWines = (props) => {
+  const { allOfMyWines } = this.props;
+  return (
+    <div className="element-list">
+      {allOfMyWines.map((wine) => {
+        return (
+          <article key={wine.id} className="single-element">
+            <Link to={`/wines/${wine.id}`} key={wine.id}>
+              <img width="150px" src={wine.imageUrl} />
+              <h2>{wine.name}</h2>
+            </Link>
+            <div>
+              <button className="add-to-cart">Add to cart</button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
 const mapState = (storeState) => {
   return {
