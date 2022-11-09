741bc4795c7433fc83d050b3a8bee540673ca758
Jackie Manginelli <jmanginelli11@gmail.com>
2022-11-09 12:29:18 -0500

    Fixed single wine view

--- client/store/index.js
+++ client/store/index.js
@@ -1,17 +1,18 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import auth from "./auth";
 import winesReducer from "./wines";
-import cheeseReducer from './cheeses'
+import cheeseReducer from "./cheeses";
+import singleWineReducer from "./singleWine";
 
-
-const reducer = combineReducers({ 
+const reducer = combineReducers({
   auth,
   cheeseReducer,
   wines: winesReducer,
- })
+  singleWineReducer,
+});
 
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
