cea3673c1ae1ab36c927f53af3be8ebe1c37de15
Anahis Valenzuela <Anahisvq7@gmail.com>
2022-11-09 09:51:22 -0500

    made tests for get all cheese route

--- server/db/index.js
+++ server/db/index.js
@@ -9,8 +9,8 @@ const User = require("./models/User");
 
 //associations could go here!
 
-Cheese.hasMany(Wine);
-Wine.hasMany(Cheese);
+Cheese.hasOne(Wine);
+Wine.hasOne(Cheese);
 
 module.exports = {
   db,
