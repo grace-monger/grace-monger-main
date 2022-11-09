c7e77ed73e364312f1217c5087177b984e8c3cf7
aphantha <alisonphanthavong@gmail.com>
2022-11-09 13:10:19 -0500

    removed logs

--- server/api/order.js
+++ server/api/order.js
@@ -1,9 +1,6 @@
 const orderRouter = require("express").Router();
 const { Order } = require("../db");
-
-// console.log("cheese", Cheese)
-
-// Get route for all cheeses 
+ 
 orderRouter.get("/", async (req, res, next) => {
   try {
     const order = await Order.create();
