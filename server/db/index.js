99808133e99260f4f1be35c0c4799a25729db240
aphantha <alisonphanthavong@gmail.com>
2022-11-09 12:36:09 -0500

    Feature: add order model and associations

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,7 @@ const db = require("./db");
 
 const Cheese = require("./models/Cheese");
 const Wine = require("./models/Wine");
+const Order = require('./models/Order')
 
 const User = require("./models/User");
 
@@ -14,9 +15,14 @@ Wine.belongsTo(Cheese);
 Wine.hasOne(Cheese)
 Cheese.belongsTo(Wine)
 
+User.hasMany(Order)
+Order.belongsToMany(Cheese, { through: 'Order_Cheese'})
+Order.belongsToMany(Wine, { through: 'Order_Wine'})
+
 module.exports = {
   db,
   User,
   Cheese,
   Wine,
+  Order
 };
