797dc1967fc665e76be8a3ba1091361f5d2c3437
aphantha <alisonphanthavong@gmail.com>
2022-11-10 16:45:21 -0500

    Feature seed data for cart

---
+++ script/cartData.js
@@ -0,0 +1,16 @@
+const cartData = [
+{
+    fulfilled: false,
+    userId: 1
+}, 
+{
+    fulfilled: false,
+    userId: 2
+}, 
+{
+    fulfilled: false,
+    userId: 3
+}
+]
+
+module.exports = cartData
