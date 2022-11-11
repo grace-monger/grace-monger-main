97580c7e6587ea399092efecc1d915e9129fec6f
Jackie Manginelli <jmanginelli11@gmail.com>
2022-11-11 12:11:36 -0500

    Feature: Working get and delete all cart routes

--- server/api/order.js
+++ server/api/order.js
@@ -15,14 +15,27 @@ orderRouter.get("/:id", async (req, res, next) => {
         through: "Order_Wine",
       },
     });
-    // console.log(userOrder);
-    // const wineOrders = await Order_Wines.findAll({
-    //   where: {
-    //     orderId: userOrder.order.dataValues.id,
-    //   },
-    // });
-    // console.log(userOrder)
     res.status(200).send(userOrder);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete entire cart route - not just one single item
+orderRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const order = await Order.findOne({
+      where: {
+        userId: req.params.id,
+        fulfilled: false,
+      },
+      include: {
+        model: Wine,
+        through: "Order_Wine",
+      },
+    });
+    await order.destroy();
+    res.send(order);
   } catch (error) {
     next(error);
   }
@@ -41,15 +54,5 @@ orderRouter.get("/:id", async (req, res, next) => {
 
 // want to remove the product from the order section of the database not from the whole database
 // getting confused by grabbing the id ? which id are we grabbing exactly - we have to find out what data we are working with when we get to the cart view
-orderRouter.delete("/:id", async (req, res, next) => {
-  try {
-    // const order = await Order.findOne({
-    //   where:
-    // })
-    await order.destroy();
-  } catch (error) {
-    next(error);
-  }
-});
 
 module.exports = orderRouter;
