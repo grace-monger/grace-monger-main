8d86c3f50f25cfc016f8f7815ad9c653f7144b93
Jackie Manginelli <jmanginelli11@gmail.com>
2022-11-11 12:14:01 -0500

    fixed second error

--- client/components/Order.js
+++ client/components/Order.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 // import { Link } from 'react-router-dom';
 import { fetchOrder } from "../store/order";
-import { me } from "../store";
+// import { me } from "../store";
 
 /**
  * COMPONENT
