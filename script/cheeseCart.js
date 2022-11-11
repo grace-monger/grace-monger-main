0a90d1531116a568fd5e9207c83312e70b5e3c29
aphantha <alisonphanthavong@gmail.com>
2022-11-11 16:24:57 -0500

    Feature: seeding order cheese table

---
+++ script/cheeseCart.js
@@ -0,0 +1,32 @@
+const cheeseCart = [
+    {
+        orderId: 1,
+        cheeseId: 10
+    }, 
+    {
+        orderId: 1,
+        cheeseId: 9
+    },
+    {
+        orderId: 1,
+        cheeseId: 8
+    },
+    {
+        orderId: 2,
+        cheeseId: 7
+    },
+    {
+        orderId: 2,
+        cheeseId: 6
+    },
+    {
+        orderId: 3,
+        cheeseId: 5
+    },
+    {
+        orderId: 3,
+        cheeseId: 4
+    }
+    ]
+    
+    module.exports = cheeseCart
--- server/db/models/Order_Cheese.js
+++ server/db/models/Order_Cheese.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const db = require("../db");
 
-const Order_Wine = db.define("Order_Wine");
+const Order_Cheese = db.define("Order_Cheese");
 
-module.exports = Order_Wine;
+module.exports = Order_Cheese;
