256189199ddba08e669791169f1749a9d5fed7d3
Jackie Manginelli <jmanginelli11@gmail.com>
2022-11-13 16:06:36 -0500

    clear cart works?

--- server/api/order.js
+++ server/api/order.js
@@ -1,4 +1,5 @@
 const orderRouter = require("express").Router();
+const { reset } = require("nodemon");
 const { Order, Cheese, Wine, Order_Wine, Order_Cheese } = require("../db");
 
 orderRouter.get("/:id", async (req, res, next) => {
@@ -34,37 +35,60 @@ orderRouter.get("/:id", async (req, res, next) => {
 });
 
 // Delete entire cart route - not just one single item
+// orderRouter.delete("/:id", async (req, res, next) => {
+//   try {
+//     const order = await Order.findByPk(req.params.id);
+//     // const cheeseOrder = await Order.findAll({
+//     //   where: {
+//     //     userId: req.params.id,
+//     //     fulfilled: false,
+//     //   },
+//     //   include: {
+//     //     model: Cheese,
+//     //     through: "Order_Cheese",
+//     //   },
+//     // });
+//     // const wineOrder = await Order.findAll({
+//     //   where: {
+//     //     userId: req.params.id,
+//     //     fulfilled: false,
+//     //   },
+//     //   include: {
+//     //     model: Wine,
+//     //     through: "Order_Wine",
+//     //   },
+//     // });
+//     // await cheeseOrder.destroy();
+//     // await wineOrder.destroy();
+//     await order.destroy();
+//     res.send(order);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
 orderRouter.delete("/:id", async (req, res, next) => {
   try {
-    const order = await Order.findByPk(req.params.id);
-    // const cheeseOrder = await Order.findAll({
-    //   where: {
-    //     userId: req.params.id,
-    //     fulfilled: false,
-    //   },
-    //   include: {
-    //     model: Cheese,
-    //     through: "Order_Cheese",
-    //   },
-    // });
-    // const wineOrder = await Order.findAll({
-    //   where: {
-    //     userId: req.params.id,
-    //     fulfilled: false,
-    //   },
-    //   include: {
-    //     model: Wine,
-    //     through: "Order_Wine",
-    //   },
-    // });
-    // await cheeseOrder.destroy();
-    // await wineOrder.destroy();
-    await order.destroy();
+    const cheeseOrder = await Order_Cheese.findByPk(req.params.id);
+    const wineOrder = await Order_Wine.findByPk(req.params.id);
+    await wineOrder.destroy();
+    await cheeseOrder.destroy();
+    const order = [wineOrder, cheeseOrder];
     res.send(order);
   } catch (error) {
     next(error);
   }
 });
+
+// orderRouter.delete("/:id", async (req, res, next) => {
+//   try {
+//     const wineOrder = await Order_Wine.findByPk(req.params.id);
+//     await wineOrder.destroy();
+//     res.send(wineOrder);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // want to remove the product from the order section of the database not from the whole database
 // getting confused by grabbing the id ? which id are we grabbing exactly - we have to find out what data we are working with when we get to the cart view
