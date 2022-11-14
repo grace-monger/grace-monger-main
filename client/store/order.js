0573208de00c91fd493894d54827631b85394a08
Jackie Manginelli <jmanginelli11@gmail.com>
2022-11-14 17:49:24 -0500

    working delete routes for wine and cheese - with bug

--- client/store/order.js
+++ client/store/order.js
@@ -5,7 +5,8 @@ const GET_ORDER = "GET_ORDER";
 const ADD_CHEESE_ORDER = "ADD_CHEESE_ORDER";
 const ADD_WINE_ORDER = "ADD_WINE_ORDER";
 // const UPDATE_CHEESE_QUANTITY = "UPDATE_CHEESE_QUANTITY";
-const CLEAR_ORDER = "CLEAR_ORDER";
+const REMOVE_WINE_ORDER = "REMOVE_WINE_ORDER";
+const REMOVE_CHEESE_ORDER = "REMOVE_CHEESE_ORDER";
 
 //ACTION CREATORS
 const getOrder = (order) => {
@@ -29,12 +30,26 @@ const addWineOrder = (wineOrder) => {
   };
 };
 
-const _clearOrder = (order) => {
+const removeWineOrder = (wineOrder) => {
   return {
-    type: CLEAR_ORDER,
-    order,
+    type: REMOVE_WINE_ORDER,
+    wineOrder,
   };
 };
+
+const removeCheeseOrder = (cheeseOrder) => {
+  return {
+    type: REMOVE_CHEESE_ORDER,
+    cheeseOrder,
+  };
+};
+
+// const _clearOrder = (order) => {
+//   return {
+//     type: CLEAR_ORDER,
+//     order,
+//   };
+// };
 
 // // info should include orderId, productId, and quantity
 // const updateCheeseQuantity = (infoToUpdate) => {
@@ -79,12 +94,37 @@ export const addNewWineOrderThunk = (orderInfo) => {
   };
 };
 
-export const clearOrder = (id) => {
+export const removeWineOrderThunk = (id) => {
+  console.log("HERE IS ID", id);
   return async (dispatch) => {
-    const { data: order } = await axios.delete(`/api/order/${id}`);
-    dispatch(_clearOrder(order));
+    try {
+      const { data: order } = await axios.delete(`/api/order/removeWine/${id}`);
+      dispatch(removeWineOrder(order));
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
+
+export const removeCheeseOrderThunk = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data: order } = await axios.delete(
+        `/api/order/removeCheese/${id}`
+      );
+      dispatch(removeCheeseOrder(order));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+// export const clearOrder = (id) => {
+//   return async (dispatch) => {
+//     const { data: order } = await axios.delete(`/api/order/${id}`);
+//     dispatch(_clearOrder(order));
+//   };
+// };
 
 // // infoToUpdate is productId, OrderId, and quantity
 // export const updateCheeseQuantityThunk = (infoToUpdate) => {
@@ -109,9 +149,13 @@ export default function orderReducer(state = initialState, action) {
       return [...state, action.cheeseOrder];
     case ADD_WINE_ORDER:
       return [...state, action.wineOrder];
-    case CLEAR_ORDER:
-      return state.filter((order) => order.id !== action.order.id);
-    // case UPDATE_CHEESE_QUANTITY: 
+    case REMOVE_WINE_ORDER:
+      return state[0].filter((wine) => wine.id !== action.wineOrder.id);
+    case REMOVE_CHEESE_ORDER:
+      return state[1].filter((cheese) => cheese.id !== action.cheeseOrder.id);
+    // case CLEAR_ORDER:
+    //   return state.filter((order) => order.id !== action.order.id);
+    // case UPDATE_CHEESE_QUANTITY:
     //   return [...state, action.infoToUpdate]
     default:
       return state;
--- server/api/order.js
+++ server/api/order.js
@@ -4,8 +4,6 @@ const { Order, Cheese, Wine, Order_Wine, Order_Cheese } = require("../db");
 
 orderRouter.get("/:id", async (req, res, next) => {
   try {
-    // need to query order table with user id using req.params.id
-    // need to query Order_Wines table by order ID and get all of those wines in that table
     const wineOrder = await Order.findAll({
       where: {
         userId: req.params.id,
@@ -34,31 +32,68 @@ orderRouter.get("/:id", async (req, res, next) => {
   }
 });
 
-orderRouter.delete("/:id", async (req, res, next) => {
-  try {
-    const cheeseOrder = await Order_Cheese.findByPk(req.params.id);
-    const wineOrder = await Order_Wine.findByPk(req.params.id);
-    await wineOrder.destroy();
-    await cheeseOrder.destroy();
-    const order = [wineOrder, cheeseOrder];
-    res.send(order);
-  } catch (error) {
-    next(error);
-  }
-});
-
 // orderRouter.delete("/:id", async (req, res, next) => {
 //   try {
+//     const cheeseOrder = await Order_Cheese.findByPk(req.params.id);
 //     const wineOrder = await Order_Wine.findByPk(req.params.id);
 //     await wineOrder.destroy();
-//     res.send(wineOrder);
+//     await cheeseOrder.destroy();
+//     const order = [wineOrder, cheeseOrder];
+//     res.send(order);
 //   } catch (error) {
 //     next(error);
 //   }
 // });
 
-// want to remove the product from the order section of the database not from the whole database
-// getting confused by grabbing the id ? which id are we grabbing exactly - we have to find out what data we are working with when we get to the cart view
+orderRouter.get("/:id", async (req, res, next) => {
+  try {
+    const wineOrder = await Order_Wine.findOne({
+      where: {
+        orderId: req.body.orderId,
+        wineId: req.params.id,
+      },
+    });
+    res.status(200).send(wineOrder);
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.delete("/removeWine/:id", async (req, res, next) => {
+  const array = req.params.id.split("-");
+  const orderId = array[0];
+  const productId = array[1];
+  try {
+    const wineOrder = await Order_Wine.findOne({
+      where: {
+        orderId: orderId,
+        wineId: productId,
+      },
+    });
+    await wineOrder.destroy();
+    res.send(wineOrder);
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.delete("/removeCheese/:id", async (req, res, next) => {
+  const array = req.params.id.split("-");
+  const orderId = array[0];
+  const productId = array[1];
+  try {
+    const cheeseOrder = await Order_Cheese.findOne({
+      where: {
+        orderId: orderId,
+        cheeseId: productId,
+      },
+    });
+    await cheeseOrder.destroy();
+    res.send(cheeseOrder);
+  } catch (error) {
+    next(error);
+  }
+});
 
 orderRouter.post("/cheese", async (req, res, next) => {
   const user = req.body.userId;
