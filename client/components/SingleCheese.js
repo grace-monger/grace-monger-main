7c62e1b21d19e4402a6b87e9ed0a3e4696b35ecc
aphantha <alisonphanthavong@gmail.com>
2022-11-14 21:52:56 -0500

    Feature: working guest checkout

--- client/components/SingleCheese.js
+++ client/components/SingleCheese.js
@@ -6,15 +6,58 @@ import { addNewCheeseOrderThunk } from "../store/order";
 import EditCheese from "./EditCheese";
 
 const SingleCheese = (props) => {
+  let [cart, setCart] = useState([]);
+  let [quantity, setQuantity] = useState("");
+
+  let localCart = localStorage.getItem("cart");
+
+  const addToGuestCart = (item) => {
+    let cartCopy = [...cart];
+    let { id } = item;
+    let existingItem = cartCopy.find((cartItem) => cartItem.id == id);
+
+    if (existingItem) {
+      existingItem.quantity += item.quantity;
+    } else {
+      cartCopy.push(item);
+    }
+
+    setCart(cartCopy);
+
+    let stringCart = JSON.stringify(cartCopy);
+    localStorage.setItem("cart", stringCart);
+  };
+
   useEffect(() => {
     props.getSingleCheeseThunk(props.match.params.id);
+    localCart = JSON.parse(localCart);
+
+    if (localCart) {
+      setCart(localCart);
+    }
   }, []);
 
   const handleClick = () => {
-    // add a thunk here to add product id and userId
-    const userId = props.userId;
-    const productId = props.match.params.id;
-    props.addNewCheeseOrderThunk({ userId, productId });
+    if (props.isLoggedIn) {
+      // add a thunk here to add product id and userId
+      const userId = props.userId;
+      const productId = props.match.params.id;
+      props.addNewCheeseOrderThunk({ userId, productId });
+    } else {
+      addToGuestCart({
+        id: props.singleCheese.id,
+        name: props.singleCheese.name,
+        imageUrl: props.singleCheese.imageUrl,
+        type: "cheese",
+        quantity: parseInt(quantity),
+      });
+    }
+  };
+
+  const handleChange = (event) => {
+    if (event.target.className === "quantity-incrementor") {
+      setQuantity(event.target.value);
+    }
   };
 
   const { singleCheese } = props;
@@ -32,7 +75,13 @@ const SingleCheese = (props) => {
       <p> Family {singleCheese.family}</p>
       <h3> {singleCheese.price}</h3>
       <p>{singleCheese.description}</p>
-      <input type="number" min="0" step="1" className="quantity-incrementor" />
+      <input
+        type="number"
+        min="0"
+        step="1"
+        className="quantity-incrementor"
+        onChange={handleChange}
+      />
       <button className="add-to-cart" onClick={handleClick}>
         Add to cart
       </button>
@@ -53,6 +102,7 @@ const mapState = (state) => {
     singleCheese: state.singleCheese,
     userId: state.auth.id,
     userType: state.auth.userType,
+    isLoggedIn: !!state.auth.id,
   };
 };
 
